2efcefc75fcdcd6bd4943f3ae8dd3dc623f495e4
antekantek <anteqqq1996@gmail.com>
2020-10-10 17:21:46 +0200

    .

--- my_app/views.py
+++ my_app/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
 from requests.compat import quote_plus
+import itertools
 
-#from . import models
-#from random import shuffle
 
 
 
@@ -29,16 +28,19 @@ def new_search(request):
     response=requests.get(final_url)
     response2=requests.get(final_url2)
     soup=BeautifulSoup(response.text,'html.parser')
-
     soup2=BeautifulSoup(response2.text, 'html.parser')
-    post_listings=soup.find_all(class_="mpof_ki mqen_m6 mp7g_oh mh36_0 mvrt_0 mg9e_8 mj7a_8 m7er_k4 _1y62o _9c44d_1I1gg")
-    post_listings2=soup2.find_all(class_="s-item   ")
 
-    print(post_listings2)
+    post_listings=soup.find_all(class_="mpof_ki mqen_m6 mp7g_oh mh36_0 mvrt_0 mg9e_8 mj7a_8 m7er_k4 _1y62o _9c44d_1I1gg")
+
+    post_listings2=soup2.find_all(class_="s-item__image-img")
+    post_listing21=soup2.find_all(class_="s-item__link")
+    post_listing22=soup2.find_all(class_="s-item__price")
+
+
 
 
     final_listings=[]
-
+    final_listings1=[]
 
     for pa in post_listings:
         post_title=pa.find(class_="_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY").text
@@ -49,26 +51,25 @@ def new_search(request):
         post_img=pa.find("img").get('data-src')
         if post_img != None:
             final_listings.append((post_title, post_url, post_price, post_img))
-    print(len(final_listings))
 
 
-    for pb in post_listings2:
-        post_title=pb.find(class_="s-item__title").text
-        print(post_title)
-        post_url = pb.find(class_="s-item__link").get('href')
-        print(post_url)
-        post_price = pb.find("span", class_="ITALIC").text.replace(",", ".").replace(" ","")
-        print(post_price)
+
+    for (ia,ib,ic) in zip(post_listings2,post_listing21,post_listing22):
+        post_title=ia.get('alt')
+        post_img = ia.get('src')
+        post_url=ib.get('href')
+        post_price=ic.find("span",class_="ITALIC").get_text().replace(",", ".")
+
         post_price = post_price[:-2]
         if len(post_price)>=8:
             post_price=post_price[6:]
         post_price = float(post_price)
-        post_img=pb.find(class_="img").get('src')
+
+        final_listings1.append((post_title, post_url, post_price, post_img))
+
+    final_listings = final_listings + final_listings1
 
 
-
-        final_listings.append((post_title, post_url, post_price, post_img))
-    print(len(final_listings))
 
 
 
